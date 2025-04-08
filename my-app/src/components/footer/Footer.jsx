7acee04924d32f2beac531a0adae427599f8517d
Baiel <tynystanovbaiel@gmail.com>
2025-04-08 20:46:55 +0600

    edit requisite test3

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import './footer.css'
 import inst from "./../../img/icon/instagram.png"
 import face from "./../../img/icon/facebook.png"
+import twit from "./../../img/icon/twitter.png"
 
 
 const Footer = () => {
@@ -16,6 +17,7 @@ const Footer = () => {
                 <div className="social__btn">
                     <a href="https://www.instagram.com/gispro.kg/"><img src= {inst} alt="" /></a>
                     <a href="https://www.facebook.com/people/Gis-Pro-Kg/pfbid02cBu35SL2TeGDx8wRK5uigZs46pDcdJHMsbZHjjdhuSLbZCHE8nwULsMrk1yoCyhEl/?refsrc=deprecated" ><img src= {face} alt="" /></a>
+                    <a href="https://x.com/gispro_kg/"><img src={ twit } alt="" /></a>
                 </div>
             </div>
         </div>
--- my-app/src/components/requisite/Requisite.jsx
+++ my-app/src/components/requisite/Requisite.jsx
@@ -1,36 +1,66 @@
 import Map from '../mapcard/Map';
-import './requisite.css'
+import './requisite.css';
+import inst from "./../../img/icon/instagram.png";
+import face from "./../../img/icon/facebook.png";
+import twit from "./../../img/icon/twitter.png";
 
 const Requisite = () => {
     return ( 
         <section className="requisite" id="requisite">
             <div className="container">
                 <div className="requi__header">
-
-                <h2 className="title-2">Реквизиты</h2>
-
+                    <h2 className="title-2">Реквизиты</h2>
                 </div>
                 
-                    <div className="requi__content">
-
-                    <div className="requi__text">
+                <div className="requi__content">
+                    <div className="requi__block">
+                        <h3>Наши контакты</h3>
                         <ul>
-                        <li><span>📍</span> г. Бишкек, ул. Токтоналиева 4Б</li>
-                        <li><span>📬</span> gisprokg@gmail.com</li>
-                        <li><span>📞</span>+996 700 600 645</li>
-                        <li><span>📞</span>+996 700 600 645</li>
-                        <li><span>📞</span>+996 700 600 645</li>
+                            <li><span>📬</span> gisprokg@gmail.com</li>
+                            <li><span>📞</span>+996 700 600 645</li>
+                            <li><span>📞</span>+996 700 600 645</li>
+                            <li><span>📞</span>+996 700 600 645</li>
                         </ul>
                     </div>
+                    <div className="requi__block">
+                        <h3>Наш адрес</h3>
+                        <ul>
+                            <li><span>📍</span> г. Бишкек, ул. Токтоналиева 4Б</li>
+                        </ul>
+                    </div>
+                    <div className="requi__block">
+                        <h3>Наши социальные сети</h3>
+                        <ul className="social-links">
+                            <li>
+                                <a href="https://www.instagram.com/gispro.kg/" target="_blank" rel="noopener noreferrer">
+                                    <img src={inst} alt="Instagram" />
+                                </a>
+                                Instagram
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/gispro.kg/" target="_blank" rel="noopener noreferrer">
+                                    <img src={face} alt="Facebook" />
+                                </a>
+                                Facebook
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/gispro_kg/" target="_blank" rel="noopener noreferrer">
+                                    <img src={twit} alt="Twitter" />
+                                </a>
+                                Twitter
+                            </li>
+                        </ul>
+                    </div>
+
                     <div className="requi__map">
-
-                    <Map />
-
+                        <Map />
                     </div>
                 </div>
             </div>
         </section>
-     );
+    );
 }
- 
-export default Requisite
+
+export default Requisite;
+
+
